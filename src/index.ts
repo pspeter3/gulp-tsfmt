88ea08693d7cf6bccd27a61ffff887064387fe9d
Phips Peter <pspeter333@gmail.com>
2015-04-04 16:17:19 -0700

    Adding an initial plugin
    This plugin works and formats the files but will be difficult to use
    outside of this code base. I should modify the interfaces so that way
    someone does not have to require TypeScript.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,100 @@
-export var test = true;
+import gutil = require("gulp-util");
+import path = require("path");
+import through = require("through2");
+import ts = require("typescript");
+
+var PLUGIN_NAME = "gulp-tsfmt";
+
+var DEFAULTS: {options?: ts.FormatCodeOptions; target?: ts.ScriptTarget} = {
+    options: {
+        IndentSize: 4,
+        TabSize: 4,
+        NewLineCharacter: "\r\n",
+        ConvertTabsToSpaces: true,
+        InsertSpaceAfterCommaDelimiter: true,
+        InsertSpaceAfterSemicolonInForStatements: true,
+        InsertSpaceBeforeAndAfterBinaryOperators: true,
+        InsertSpaceAfterKeywordsInControlFlowStatements: true,
+        InsertSpaceAfterFunctionKeywordForAnonymousFunctions: false,
+        InsertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: false,
+        PlaceOpenBraceOnNewLineForFunctions: false,
+        PlaceOpenBraceOnNewLineForControlBlocks: false
+    },
+    target: ts.ScriptTarget.ES5
+};
+
+class Transformer {
+    private _formatter: Formatter;
+    private _options: ts.FormatCodeOptions;
+    private _rulesProvider: RulesProvider;
+    private _target: ts.ScriptTarget;
+
+    constructor(options: ts.FormatCodeOptions, target: ts.ScriptTarget) {
+        this._formatter = (<{formatting?: Formatter}>ts).formatting;
+        this._options = options;
+        this._rulesProvider = new this._formatter.RulesProvider();
+        this._rulesProvider.ensureUpToDate(this._options);
+        this._target = target;
+    }
+
+    process(file: VinylFile): void {
+        var source = this._toSourceFile(file);
+        var edits = this._formatSource(source);
+        file.contents = this._applyEdits(file, edits);
+    }
+
+    private _toSourceFile(file: VinylFile): ts.SourceFile {
+        return ts.createSourceFile(path.basename(file.path), file.contents.toString(), this._target, ts.servicesVersion);
+    }
+
+    private _formatSource(source: ts.SourceFile): ts.TextChange[] {
+        return this._formatter.formatDocument(source, this._rulesProvider, this._options);
+    }
+
+    private _applyEdits(file: VinylFile, edits: ts.TextChange[]): Buffer {
+        return edits.reduceRight((contents, edit) => {
+            var head = contents.slice(0, edit.span.start());
+            var tail = contents.slice(edit.span.start() + edit.span.length());
+            var change = new Buffer(edit.newText, "utf8");
+            return Buffer.concat([head, change, tail]);
+        }, file.contents);
+    }
+}
+
+function format(params?: {options?: ts.FormatCodeOptions; target?: ts.ScriptTarget}): NodeJS.ReadWriteStream {
+    params = params || DEFAULTS;
+    var options = params.options || DEFAULTS.options;
+    var target = params.target || DEFAULTS.target;
+    var transformer = new Transformer(options, target);
+    return through.obj(function (file: VinylFile, encoding: string, callback: () => void) {
+        var stream = <NodeJS.ReadWriteStream>this;
+        if (file.isStream()) {
+            stream.emit("error", new gutil.PluginError(PLUGIN_NAME, "Streams are not supported"));
+        }
+        if (file.isBuffer()) {
+            transformer.process(file);
+        }
+        (<any>stream).push(file);
+        callback();
+    });
+}
+
+interface Formatter {
+    /* tslint:disable */
+    RulesProvider: { new(): RulesProvider };
+    /* tslint:enable */
+    formatDocument(source: ts.SourceFile, rulesProvider: RulesProvider, options: ts.FormatCodeOptions): ts.TextChange[];
+}
+
+interface RulesProvider {
+    ensureUpToDate(options: ts.FormatCodeOptions): void;
+}
+
+interface VinylFile {
+    path: string;
+    contents: Buffer;
+    isBuffer(): boolean;
+    isStream(): boolean;
+}
+
+export = format;
--- test/index_spec.ts
+++ test/index_spec.ts
@@ -1,10 +1,71 @@
 import chai = require("chai");
-import index = require("../src/index");
+import gutil = require("gulp-util");
+import stream = require("stream");
+import tsfmt = require("../src/index");
+import ts = require("typescript");
+import VinylFile = require("vinyl");
 
 var assert = chai.assert;
 
-describe("index", () => {
-    it("should be true", () => {
-        assert.isTrue(index.test);
+describe("gulp-tsfmt", () => {
+    describe("params", () => {
+        it("should handle no params", () => {
+            tsfmt();
+        });
+
+        it("should handle options", () => {
+            tsfmt({
+                options: {
+                    IndentSize: 4,
+                    TabSize: 4,
+                    NewLineCharacter: "\r\n",
+                    ConvertTabsToSpaces: true,
+                    InsertSpaceAfterCommaDelimiter: true,
+                    InsertSpaceAfterSemicolonInForStatements: true,
+                    InsertSpaceBeforeAndAfterBinaryOperators: true,
+                    InsertSpaceAfterKeywordsInControlFlowStatements: true,
+                    InsertSpaceAfterFunctionKeywordForAnonymousFunctions: false,
+                    InsertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: false,
+                    PlaceOpenBraceOnNewLineForFunctions: false,
+                    PlaceOpenBraceOnNewLineForControlBlocks: false
+                }
+            });
+        });
+
+        it("should handle target", () => {
+            tsfmt({
+                target: ts.ScriptTarget.ES6
+            });
+        });
+    });
+
+    describe("buffers", () => {
+        it("should format the code", (done) => {
+            var file = new VinylFile({
+                contents: new Buffer("var a=function(v:number){return 0+1+2+3;}", "utf8")
+            });
+            var formatter = tsfmt();
+            formatter.once("data", (file: VinylFile) => {
+                assert.equal(file.contents.toString(), "var a = function(v: number) { return 0 + 1 + 2 + 3; }");
+                done();
+            });
+            formatter.write(<any>file);
+        });
+    });
+
+    describe("streams", () => {
+        it("should not be supported", (done) => {
+            var file = new VinylFile({
+                contents: new stream.Readable()
+            });
+            var formatter = tsfmt();
+            formatter.once("error", (error: Error) => {
+                assert.equal(error.message, "Streams are not supported");
+                assert.equal((<{plugin?: string}>error).plugin, "gulp-tsfmt");
+                assert.instanceOf(error, gutil.PluginError);
+                done();
+            });
+            formatter.write(<any>file);
+        });
     });
 });
